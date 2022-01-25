bb454ab3e66477e6594de7753440e5f38fa41c0b
NgShiYingNicole <97488614+NgShiYingNicole@users.noreply.github.com>
2022-01-25 09:42:50 +0800

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,12 @@
+public class Time{
+	public static void main(String [] args){
+		long totalMilliseconds = System.currentTimeMillis();
+		long totalSeconds = totalMilliseconds/1000;
+		long currentSecond = totalMilliseconds%60;
+		long totalMinutes = totalSeconds/60;
+		long currentMinute = totalMinutes%60;
+		long totalHours = totalMinutes/60;
+		long currentHour = totalHours%24;
+		System.out.format("Current time is %d:%d:%d GMT",currentHour,currentMinute,currentSecond);
+	}
+}
